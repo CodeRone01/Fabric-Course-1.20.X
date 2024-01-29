2b98cb4ff5754ecbf08330dacc20d0aa48443bc6
Coderone01 <corvicrew5@outlook.com>
2024-01-28 20:28:49 -0800

    added a comment

---
+++ src/main/java/net/rone01/mccourse/MCCourseMod.java
@@ -0,0 +1,17 @@
+package net.rone01.mccourse;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MCCourseMod implements ModInitializer {
+	public static final String MOD_ID = "mccourse";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// Very important comment
+		LOGGER.info("Hello Fabric world!");
+	}
+}
